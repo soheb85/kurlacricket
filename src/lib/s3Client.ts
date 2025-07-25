cf65c8256c14d3c5f7e208fb5ae0c82f1b67d3fc
soheb85 <shoebkhan3145@gmail.com>
2025-07-25 15:17:49 +0530

    update S3

--- src/lib/s3Client.ts
+++ src/lib/s3Client.ts
@@ -3,7 +3,7 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
   region: process.env.AWS_REGION as string,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 });
